b284c5e3c837b18fed5833ef1ec38f5e644a0df7
NavaneethKaila <navaneeth.kaila@gmail.com>
2021-10-27 09:56:16 +0000

    errors

---
+++ src/components/Login/index.js
@@ -0,0 +1,14 @@
+import {Component} from 'react'
+import './index.css'
+
+class Login extends Component {
+  render() {
+    return (
+      <>
+        <h1>Hi</h1>
+      </>
+    )
+  }
+}
+
+export default Login
